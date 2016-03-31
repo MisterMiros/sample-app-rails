12260e0f3f92d394df2794cfb18e284bf6f15278
MisterMiros <mistermiros@gmail.com>
2016-03-31 21:41:31 +0500

    Finish user authentication

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
   it { should be_valid }
 
@@ -58,12 +59,12 @@ describe User do
       it { should_not be_valid }
     end
     describe "is saved to base" do
-      let(:mixed_case_email) { "USER@example.com"}
+      let(:mixed_case_email) { "USER@example.com" }
       it "should be in lowercase" do
         @user.email = mixed_case_email
         @user.save
         expect(@user.reload.email).to eq mixed_case_email.downcase
-       end
+      end
     end
   end
 
@@ -96,5 +97,10 @@ describe User do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be false }
     end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    it { expect(@user.remember_token).not_to be_blank }
   end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,34 @@
 include ApplicationHelper
+
+def fill_with_valid_signin(user)
+  fill_in "Email", with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+def fill_with_valid_signup
+  fill_in "Name", with: "Example User"
+  fill_in "Email", with: "user@example.com"
+  fill_in "Password", with: "foobar"
+  fill_in "Confirmation", with: "foobar"
+end
+
+def danger
+  'div.alert.alert-danger'
+end
+
+def success
+  'div.alert.alert-success'
+end
+
+RSpec::Matchers.define :have_error_message do |message| 
+  match do |page|
+    expect(page).to have_selector(danger, text: message)
+  end
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    expect(page).to have_selector(success, text: message)
+  end
+end
