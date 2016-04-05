235b6dc6fe503e66b330e78b5c2fac8b068127fb
MisterMiros <mistermiros@gmail.com>
2016-04-05 17:25:44 +0500

    Add relationship to user model, add tests for relationship model

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -3,10 +3,21 @@ require 'spec_helper'
 
 describe Relationship do
   let(:follower) { FactoryGirl.create(:user) }
-  let(:follower) { FactoryGirl.create(:user) }
+  let(:followed) { FactoryGirl.create(:user) }
   let(:relationship) { follower.relationships.build(followed_id: followed.id) }
 
   subject { relationship }
 
   it { should be_valid }
+
+  describe "follower methods" do
+    it { should respond_to(:follower) }
+    it { should respond_to(:followed) }
+    it "follower should be equal to follower user" do
+      expect(relationship.follower).to eq follower
+    end
+    it "followed should be equal to followed user" do
+      expect(relationship.followed).to eq followed
+    end
+  end
 end
